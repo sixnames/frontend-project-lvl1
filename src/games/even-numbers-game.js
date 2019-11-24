ccddab0da95c71e79d58f06b3177f844a1313de7
sixnames <slava.kirshman@icloud.com>
2019-11-24 09:15:31 +0200

    brain-even game success value simplified.

--- src/games/even-numbers-game.js
+++ src/games/even-numbers-game.js
@@ -8,7 +8,7 @@ const round = () => {
   const isEven = number % 2 === 0;
   const correctAnswer = isEven ? 'yes' : 'no';
   return {
-    success: (isEven && answer === 'yes') || (!isEven && answer === 'no'),
+    success: correctAnswer === answer,
     answer,
     correctAnswer,
   };
