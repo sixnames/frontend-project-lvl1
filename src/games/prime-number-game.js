068f7cd32b8ba3c9085fe2c3abdd3583b65f0af6
sixnames <slava.kirshman@icloud.com>
2019-11-28 20:16:41 +0200

    isPrime function added to the prime game.

--- src/games/prime-number-game.js
+++ src/games/prime-number-game.js
@@ -1,8 +1,14 @@
 import createGame from '../utils/createGame';
 import getRandomNumber from '../utils/getRandomNumber';
-import isPrime from '../utils/isPrime';
 
 const gameRules = 'Answer "yes" if the number is prime, otherwise answer "no".';
+
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
 
 const round = ({ askQuestion }) => {
   const number = getRandomNumber();
