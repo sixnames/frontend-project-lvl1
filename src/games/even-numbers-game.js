baaeed9cf7e5c909323238ce2e55128b1182978d
sixnames <slava.kirshman@icloud.com>
2019-12-05 08:59:08 +0200

    even-numbers-game refactored.

--- src/games/even-numbers-game.js
+++ src/games/even-numbers-game.js
@@ -5,9 +5,9 @@ const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const round = ({ askQuestion }) => {
-  const number = getRandomNumber();
-  const answer = askQuestion(number);
-  const isEvenNumber = isEven(number);
+  const roundQuestion = getRandomNumber();
+  const answer = askQuestion(roundQuestion);
+  const isEvenNumber = isEven(roundQuestion);
   const correctAnswer = isEvenNumber ? 'yes' : 'no';
   return {
     success: correctAnswer === answer,
