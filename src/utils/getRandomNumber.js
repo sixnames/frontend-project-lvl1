fdf463928c4835232068c7e12a1db805f5e68f51
sixnames <slava.kirshman@icloud.com>
2019-12-12 08:21:44 +0200

    getRandomNumber refactored to the arrow function.

--- src/utils/getRandomNumber.js
+++ src/utils/getRandomNumber.js
@@ -1,8 +1,8 @@
-function getRandomNumber(min = 1, max = 100) {
+const getRandomNumber = (min = 1, max = 100) => {
   const minValue = Math.floor(min);
   const maxValue = Math.floor(max);
   
   return Math.floor((Math.random() * (maxValue - minValue)) + minValue);
-}
+};
 
 export default getRandomNumber;
